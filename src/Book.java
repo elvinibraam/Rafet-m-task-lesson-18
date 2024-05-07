0c8aa58bf4c5b31489ed13ff1217b8c39762ce5c
Elvin Ibrahimov <elvin.ibrhmv@gmail.com>
2024-05-07 21:48:37 +0400

    Task bitdi

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,13 @@
 public class Book {
+    public Book() {
+    }
+
+    public Book(String name, int price, int count){
+        this.name=name;
+        this.price=price;
+        this. count=count;
+    }
+    public String name;
+    public int price;
+    public int count;
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,61 @@
+import java.util.Scanner;
+
 public class Main {
+    static Book[] books = new Book[2];
+    static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        while (true){
+            System.out.println("Secim edin");
+            System.out.println("1- Butun kitablara bax \n2- Kitab elave et");
+            Book book = new Book();
+            int input = scanner.nextInt();
+            scanner.nextLine();
+            switch (input) {
+                case 1:
+                    showBook();
+                    break;
+                case 2:
+                    addBook();
+
+            }
+        }
+
+
+    }
+
+    public static void showBook() {
+        for (Book book : books) {
+            if(book!=null){
+                System.out.println("Name: " + book.name + "Price" + book.price + "Count" + book.count);
+
+            }
+        }
+
+    }
+
+    public static void addBook() {
+        String bookName = scanner.nextLine();
+        int bookPrice = scanner.nextInt();
+        int bookCount = scanner.nextInt();
+        Book book = new Book("Ali ve Nino", 20, 2);
+        boolean hasEmpty = true;
+        for (int i = 0; i < books.length; i++) {
+            if (books[i] == null) {
+                books[i] = book;
+                break;
+            }
+        }
+        if (hasEmpty) {
+            Book[] newBook = new Book[books.length * 2];
+            for (int i = 0; i < books.length; i++) {
+                newBook[i] = books[i];
+            }
+            newBook[books.length] = book;
+            books = newBook;
+
+        }
+
     }
 }
